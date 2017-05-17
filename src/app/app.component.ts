5a2f45473226cb42d3b522be7847cf83f9194863
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-05-17 23:48:18 +0200

    Added new Component *lista-contactos*

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // Selector CSS del elemento donde se instanciará el componente
   selector: 'app-root',
+  // Ruta al template correspondiente al componente
   templateUrl: './app.component.html',
+  // Ruta al CSS correspondiente al componente
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  
+  // Los atributos de clase no necesitan ser definidos con 'let.
+  // Aunque los atributos se marquen como privados, siguen siendo
+  // visibles para el template.
+  private title: string; 
+
+  // Este método es de obligatoria implementación cuando usamos la inerfaz OnInit. 
+  // Puesto que no retorna nada, podemos anotarlo como 'void'. 
+  // Este método se ejecuta al instanciarse la clase 'AppComponent'.
+  ngOnInit(): void {
+    this.title = 'Buenas noches Keepcoders!!';
+  }
+
+  mostrarContactoSeleccionado(contacto: string): void {
+    alert(`Contacto seleccionado: ${contacto}`);
+  }
 }
---
+++ src/app/lista-contactos/lista-contactos.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'lista-contactos',
+  templateUrl: './lista-contactos.component.html',
+  styleUrls: ['./lista-contactos.component.css']
+})
+export class ListaContactosComponent implements OnInit {
+
+  @Input() contactos: string[];
+  @Output() contactoSeleccionado: EventEmitter<string>;
+
+  constructor() {
+     this.contactoSeleccionado = new EventEmitter<string>();
+   }
+
+  ngOnInit() {
+  }
+
+  notificarContactoSeleccionado(contacto: string): void {
+    this.contactoSeleccionado.emit(contacto);
+  }
+
+}
